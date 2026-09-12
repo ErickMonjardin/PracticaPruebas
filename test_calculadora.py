481e223f9369cf9ce7ef5d234e3604093d16a2e3
ErickMonjardin <erick.monjardin@uabc.edu.mx>
2026-09-11 18:47:49 -0700

    Falla prueba

--- test_calculadora.py
+++ test_calculadora.py
@@ -2,8 +2,7 @@ from calculadora import Calculadora
 
 def test_add():
     calc = Calculadora()
-    assert calc.add(2, 3) == 5
+    assert calc.add(2, 3) == 6
 def test_sub():
     calc = Calculadora()
     assert calc.sub(5, 2) == 3
-    
