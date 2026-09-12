2248e13c01215bea1f0bc77e5634730aacc66a32
ErickMonjardin <erick.monjardin@uabc.edu.mx>
2026-09-11 18:53:08 -0700

    Pruebas de falla y correccion

--- test_calculadora.py
+++ test_calculadora.py
@@ -2,7 +2,7 @@ from calculadora import Calculadora
 
 def test_add():
     calc = Calculadora()
-    assert calc.add(2, 3) == 6
+    assert calc.add(2, 3) == 5
 def test_sub():
     calc = Calculadora()
     assert calc.sub(5, 2) == 3
